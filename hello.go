893fc026f57237291ddea3b76d684e77c4dabc6f
Miriam Silver <miriam.a.silver@gmail.com>
2025-04-13 19:39:35 +0300

    creating packge

--- hello.go
+++ hello.go
@@ -2,38 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/Ki-rin/go-example/points"
 )
 
-// Magnitude is an interface for anything that has a "size"
-type Magnitude interface {
-	Abs() float64
-}
-
-// Point is a 2D point with X and Y
-type Point struct {
-	X, Y float64
-}
-
-// Abs returns the square magnitude of a point
-func (p Point) Abs() float64 {
-	return p.X*p.X + p.Y*p.Y
-}
-
-// Circle is a shape with a radius
-type Circle struct {
-	Radius float64
-}
-
-// Abs returns the area of the circle (πr² simplified as r*r for this example)
-func (c Circle) Abs() float64 {
-	return c.Radius * c.Radius
-}
-
 func main() {
-	objects := []Magnitude{
-		Point{3, 4},       // Abs = 3*3 + 4*4 = 25
-		Circle{Radius: 5}, // Abs = 5*5 = 25
-		Point{5, 12},      // Abs = 25 + 144 = 169
+	objects := []points.Magnitude{
+		points.Point{X: 3, Y: 4},  // Abs = 3*3 + 4*4 = 25
+		points.Circle{Radius: 5},  // Abs = 5*5 = 25
+		points.Point{X: 5, Y: 12}, // Abs = 25 + 144 = 169
 	}
 
 	for i, obj := range objects {
