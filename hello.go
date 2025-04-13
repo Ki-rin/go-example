2d7452142d1594333efbb2dfa5d66b2f2adcd0cd
Miriam Silver <miriam.a.silver@gmail.com>
2025-04-13 18:53:16 +0300

    go routines

--- hello.go
+++ hello.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-type Point struct {
-	x, y float32
+func greet(name string, channel chan string) {
+	time.Sleep(1 * time.Second)
+	channel <- fmt.Sprintf("Hello, %s!", name)
 }
 
-func Abs(point Point) float32 {
-	return point.x*point.x + point.y*point.y
-}
-
-func (p Point) Abs() float32 {
-	return p.x*p.x + p.y*p.y
+func greetSlow(name string, channel chan string) {
+	time.Sleep(5 * time.Second)
+	channel <- fmt.Sprintf("Hello, %s!", name)
 }
 
 func main() {
-
-	p := Point{1, 2} // This is an assignment to the variable p
-	fmt.Println(p.x)
-	fmt.Println(p.Abs() != Abs(p))
+	channel := make(chan string)
+	go greet("Old World", channel)
+	go greetSlow("New World", channel)
+	for i := 0; i < 2; i++ {
+		select {
+		case message := <-channel:
+			fmt.Println(message)
+		case <-time.After(20 * time.Second):
+			fmt.Println("Timeout!")
+		}
+	}
 }
