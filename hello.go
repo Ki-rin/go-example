b3e1db6b46d2c69cbc11fb8223e35a429566d145
Miriam Silver <miriam.a.silver@gmail.com>
2025-04-13 19:15:12 +0300

    interface example

--- hello.go
+++ hello.go
@@ -2,29 +2,41 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func greet(name string, channel chan string) {
-	time.Sleep(1 * time.Second)
-	channel <- fmt.Sprintf("Hello, %s!", name)
+// Magnitude is an interface for anything that has a "size"
+type Magnitude interface {
+	Abs() float64
 }
 
-func greetSlow(name string, channel chan string) {
-	time.Sleep(5 * time.Second)
-	channel <- fmt.Sprintf("Hello, %s!", name)
+// Point is a 2D point with X and Y
+type Point struct {
+	X, Y float64
+}
+
+// Abs returns the square magnitude of a point
+func (p Point) Abs() float64 {
+	return p.X*p.X + p.Y*p.Y
+}
+
+// Circle is a shape with a radius
+type Circle struct {
+	Radius float64
+}
+
+// Abs returns the area of the circle (πr² simplified as r*r for this example)
+func (c Circle) Abs() float64 {
+	return c.Radius * c.Radius
 }
 
 func main() {
-	channel := make(chan string)
-	go greet("Old World", channel)
-	go greetSlow("New World", channel)
-	for i := 0; i < 2; i++ {
-		select {
-		case message := <-channel:
-			fmt.Println(message)
-		case <-time.After(20 * time.Second):
-			fmt.Println("Timeout!")
-		}
+	objects := []Magnitude{
+		Point{3, 4},       // Abs = 3*3 + 4*4 = 25
+		Circle{Radius: 5}, // Abs = 5*5 = 25
+		Point{5, 12},      // Abs = 25 + 144 = 169
+	}
+
+	for i, obj := range objects {
+		fmt.Printf("Object #%d magnitude: %.2f\n", i+1, obj.Abs())
 	}
 }
